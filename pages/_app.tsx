f97b04da8d15163b9585f44b1913e30cf427fd35
Rinchindugar <rinchindugar70@gmail.com>
2022-05-15 17:53:24 +0800

    feat: 🎸 implemented apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from 'lib/apolloClient';
 import '../styles/globals.css';
-import 'antd/dist/antd.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
