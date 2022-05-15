ab71abe9f25202328ea6a679ee4a916145b70498
Rinchindugar <rinchindugar70@gmail.com>
2022-05-15 16:48:53 +0800

    feat: 🎸 added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './components/**/*.tsx',
+    './pages/**/*.tsx',
+    './styles/globals.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
