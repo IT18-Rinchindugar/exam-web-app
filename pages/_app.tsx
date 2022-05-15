c473fc144eca7fd9d4d7f1250994b13b2e2c18d4
Rinchindugar <rinchindugar70@gmail.com>
2022-05-15 19:56:23 +0800

    feat: 🎸 added toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from 'lib/apolloClient';
+import { ToastContainer } from 'react-toastify';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   return (
     <ApolloProvider client={apolloClient}>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        pauseOnHover={false}
+      />
       <Component {...pageProps} />
     </ApolloProvider>
   );
